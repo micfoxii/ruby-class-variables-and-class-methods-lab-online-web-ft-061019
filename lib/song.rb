13fb1e06c19268386f6758610600069a2a77324f
Michael Fox <michael.c.fox.ii@gmail.com>
2019-06-17 14:37:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,8 +4,8 @@ class Song
   attr_accessor :name, :artist, :genre
   
   @@count = 0 
-  #@@genres = []
-  #@@artists = []
+  @@genres = []
+  @@artists = []
   
   def initialize(song_name, artist, genre)
     @name = song_name
