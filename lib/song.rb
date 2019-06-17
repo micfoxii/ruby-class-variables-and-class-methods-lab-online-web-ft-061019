60b3f623fa74660d5c470e4f5dc746b8ffa1566c
Michael Fox <michael.c.fox.ii@gmail.com>
2019-06-17 13:56:50 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,21 @@
+require 'pry'
+
+class Song 
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0 
+  #@@genres = []
+  #@@artists = []
+  
+  def initialize(song_name, artist, genre)
+    @name = song_name
+    @artist = artist
+    @genre = genre 
+    @@count += 1 
+    @@genres.push(genre)
+    @@artists.push(artist)
+  end
+  
+  
+  
+end
