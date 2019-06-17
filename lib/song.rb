6437182da4f6812808e482df0de21cf08e9e198b
Michael Fox <michael.c.fox.ii@gmail.com>
2019-06-17 14:36:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,8 +12,8 @@ class Song
     @artist = artist
     @genre = genre 
     @@count += 1 
-    @@genres.push(genre)
-    @@artists.push(artist)
+    @@genres << genre
+    @@artists << artist
   end
   
   def self.count
